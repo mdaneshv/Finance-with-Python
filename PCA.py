9c21a2193bf9ed537c4a138dc1e4b1c1c04fa284
mdaneshv <58681626+mdaneshv@users.noreply.github.com>
2020-11-25 19:08:30 -0600

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -5,16 +5,18 @@ Created on Wed Nov 13 22:00:25 2019
 @author: Mdaneshvar
 """
 
-from sklearn.datasets import fetch_mldata
+from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 
-mnist = fetch_mldata('MNIST original')
+mnist = fetch_openml("mnist_784")
 scaler = StandardScaler()
+
 # Fit on training set only.
 scaler.fit(train_img)
+
 # Apply transform to both the training set and the test set.
 train_img = scaler.transform(train_img)
 test_img = scaler.transform(test_img)
